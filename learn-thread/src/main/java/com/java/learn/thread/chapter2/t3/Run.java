e2c03614195c9b9a27da27722bf4f0b2149b5faf
honglujun <909408954@qq.com>
2018-09-29 17:14:23 +0800

    多线程0929

--- learn-thread/src/main/java/com/java/learn/thread/chapter2/t3/Run.java
+++ learn-thread/src/main/java/com/java/learn/thread/chapter2/t3/Run.java
@@ -10,10 +10,11 @@ package com.java.learn.thread.chapter2.t3;
  */
 public class Run {
     public static void main(String[] args) {
-        HasSelfPrivateNum numRef = new HasSelfPrivateNum();
-        ThreadA threadA = new ThreadA(numRef);
+        HasSelfPrivateNum numRef1 = new HasSelfPrivateNum();
+        HasSelfPrivateNum numRef2 = new HasSelfPrivateNum();
+        ThreadA threadA = new ThreadA(numRef1);
         new Thread(threadA).start();
-        ThreadB threadB = new ThreadB(numRef);
+        ThreadB threadB = new ThreadB(numRef2);
         threadB.start();
     }
 
---
+++ learn-thread/src/main/java/com/java/learn/thread/chapter2/t4/ThreadB.java
@@ -0,0 +1,25 @@
+package com.java.learn.thread.chapter2.t4;
+
+/**
+ * @Title:
+ * @Package
+ * @Description
+ * @Author 111665
+ * @CreateDate 2018/09/29/16:44
+ * @Version 1.0
+ */
+public class ThreadB extends Thread {
+
+    private MyObject myObject;
+
+    public ThreadB(MyObject myObject) {
+        super();
+        this.myObject = myObject;
+    }
+
+    @Override
+    public void run() {
+        super.run();
+        myObject.methodA();
+    }
+}
