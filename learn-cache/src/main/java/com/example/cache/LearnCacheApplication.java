052f198f4093cd479e3377582711ad613c743565
honglujun <909408954@qq.com>
2019-05-31 10:57:23 +0800

    提交代码-缓存

---
+++ learn-cache/src/main/java/com/example/cache/LearnCacheApplication.java
@@ -0,0 +1,13 @@
+package com.example.cache;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnCacheApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnCacheApplication.class, args);
+    }
+
+}
