0db8d793139bddffc9f789a5a2c253fbdab99254
honglujun <909408954@qq.com>
2019-06-04 12:02:27 +0800

    提交代码-thinkingJava第七章--第五章 7.9与5.7解释类的初始化与加载过程

--- learn-thinkingjava/src/main/java/com/example/thinking/chapter5/initialization/StaticInitialization.java
+++ learn-thinkingjava/src/main/java/com/example/thinking/chapter5/initialization/StaticInitialization.java
@@ -18,6 +18,8 @@ class Table {
         bowl2.f1(1);
     }
 
+    Bowl bowl7 = new Bowl(7);
+
     void f2(int marker) {
         System.out.println("f2(" + marker + ")");
     }
@@ -26,13 +28,14 @@ class Table {
 }
 
 class Cupboard {
-    Bowl bowl3 = new Bowl(3);
-    static Bowl bowl4 = new Bowl(4);
-
     Cupboard() {
+        bowl3.f1(9);
         System.out.println("Cupboard()");
         bowl4.f1(2);
     }
+
+    Bowl bowl3 = new Bowl(3);
+    static Bowl bowl4 = new Bowl(4);
 
     void f3(int marker) {
         System.out.println("f3(" + marker + ")");
@@ -47,33 +50,48 @@ class Cupboard {
  * @author win10
  */
 public class StaticInitialization {
+
     public static void main(String[] args) {
         System.out.println("Creating new Cupboard() in main");
-        new  Cupboard();
+        new Cupboard();
         System.out.println("Creating new Cupboard() in main");
         new Cupboard();
         table.f2(1);
         cupboard.f3(1);
     }
+
+    // 1.最先初始化
     static Table table = new Table();
+    // 2.接着初始化
     static Cupboard cupboard = new Cupboard();
+
 }
-/*Output:
+/*
+初始化先静态的对象，再非静态的对象；
+静态对象按代码的先后顺序执行；
+变量的初始化按代码的的先后顺序执行；
+构造方法在静态对象，变量初始化结束之后开始执行；
+构造方法只有到被new的时候才执行；
+Output:
 Bowl(1)
 Bowl(2)
+Bowl(7)   // 变量的初始化按代码的的先后顺序执行
 Table()
 f1(1)
 Bowl(4)
 Bowl(5)
 Bowl(3)
+f1(9)
 Cupboard()
 f1(2)
 Creating new Cupboard() in main
 Bowl(3)
+f1(9)
 Cupboard()
 f1(2)
 Creating new Cupboard() in main
 Bowl(3)
+f1(9)
 Cupboard()
 f1(2)
 f2(1)
