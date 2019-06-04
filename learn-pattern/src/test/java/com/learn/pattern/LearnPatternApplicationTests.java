e378afc8dc6d85306975f6bdca237e8d18c89801
honglujun <909408954@qq.com>
2019-06-04 17:35:34 +0800

    新建设计模式模块

---
+++ learn-pattern/src/test/java/com/learn/pattern/LearnPatternApplicationTests.java
@@ -0,0 +1,16 @@
+package com.learn.pattern;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LearnPatternApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
