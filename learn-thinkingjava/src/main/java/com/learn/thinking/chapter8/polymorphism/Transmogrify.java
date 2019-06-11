460cfc22b5393ded2805bbd3b27952d5e94eccd1
honglujun <909408954@qq.com>
2019-06-11 17:44:39 +0800

    JavaThinking第八章

---
+++ learn-thinkingjava/src/main/java/com/learn/thinking/chapter8/polymorphism/Transmogrify.java
@@ -0,0 +1,50 @@
+package com.learn.thinking.chapter8.polymorphism;
+
+class Actor {
+    public void act() {
+
+    }
+}
+
+class HappyActor extends Actor {
+    @Override
+    public void act() {
+        System.out.println("HappyActor");
+    }
+}
+
+class SadActor extends Actor {
+    @Override
+    public void act() {
+        System.out.println("SadActor");
+    }
+}
+
+class Stage {
+    private Actor actor = new HappyActor();
+    public void change(){
+        actor = new SadActor();
+    }
+    public void performPlay(){
+        actor.act();
+    }
+}
+
+/**
+ * 使完全改变
+ *
+ * @author win10
+ */
+public class Transmogrify {
+    public static void main(String[] args) {
+        Stage stage = new Stage();
+        stage.performPlay();
+        stage.change();
+        stage.performPlay();
+    }
+}
+/*
+Output:
+HappyActor
+SadActor
+ */
