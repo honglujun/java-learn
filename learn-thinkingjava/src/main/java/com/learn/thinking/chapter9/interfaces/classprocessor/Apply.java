7db7bbcd42346e70e7c0a3e5b86226e91b48b591
honglujun <909408954@qq.com>
2019-06-14 10:30:27 +0800

    9.3 完全解耦 -- 策略模式

--- learn-thinkingjava/src/main/java/com/learn/thinking/chapter9/interfaces/classprocessor/Apply.java
+++ learn-thinkingjava/src/main/java/com/learn/thinking/chapter9/interfaces/classprocessor/Apply.java
@@ -33,6 +33,15 @@ class Splitter extends Processor {
     }
 }
 
+/**
+ * 策略模式：
+ * 创建一个能够根据所传的参数对象的不同而具有不同的行为方法。
+ * 这个类方法包含所要执行的算法中固定不变的部分，
+ * “策略”包含变化的部分，就是传递进去的参数对象，它包含要执行的代码
+ * Processor对象就是一个策略
+ * main()方法中有三种不同的策略应用到了String类型的s对象上
+ *
+ */
 public class Apply {
     public static void process(Processor p, Object s) {
         System.out.println("Using Processor " + p.name());
