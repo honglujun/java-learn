64c80822eae58a41d05dba85d18fe8947dbef8f8
honglujun <909408954@qq.com>
2019-07-15 17:54:48 +0800

    14章

---
+++ learn-thinkingjava/src/main/java/com/learn/thinking/chapter14/typeinfo/pet/Pet.java
@@ -0,0 +1,7 @@
+//: typeinfo/pets/Pet.java
+package com.learn.thinking.chapter14.typeinfo.pet;
+
+public class Pet extends Individual {
+  public Pet(String name) { super(name); }
+  public Pet() { super(); }
+} ///:~
