617ecf8bb5e4cdfa99d0a937eef6a96379b2122d
honglujun <909408954@qq.com>
2019-07-16 17:32:04 +0800

    11.6迭代器

--- learn-thinkingjava/src/main/java/com/learn/thinking/chapter11/holding/CrossContainerIteration.java
+++ learn-thinkingjava/src/main/java/com/learn/thinking/chapter11/holding/CrossContainerIteration.java
@@ -13,8 +13,9 @@ public class CrossContainerIteration {
     public static void display(Iterator<Pet> it) {
         while (it.hasNext()) {
             Pet p = it.next();
-            System.out.println(p.id() + ":" + p + " ");
+            System.out.print(p.id() + ":" + p + " ");
         }
+        System.out.println();
     }
 
     public static void main(String[] args) {
@@ -28,3 +29,9 @@ public class CrossContainerIteration {
         display(petsTS.iterator());
     }
 }
+/*Output:
+0:Rat 1:Manx 2:Cymric 3:Mutt 4:Pug 5:Cymric 6:Pug 7:Manx
+0:Rat 1:Manx 2:Cymric 3:Mutt 4:Pug 5:Cymric 6:Pug 7:Manx
+0:Rat 1:Manx 2:Cymric 3:Mutt 4:Pug 5:Cymric 6:Pug 7:Manx
+5:Cymric 2:Cymric 7:Manx 1:Manx 3:Mutt 6:Pug 4:Pug 0:Rat
+ */
