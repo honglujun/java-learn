9f61d781b24f6564da94edfe3434ddc4c5444e34
honglujun <909408954@qq.com>
2019-07-19 11:09:22 +0800

    11.8Stack

---
+++ learn-thinkingjava/src/main/java/com/learn/thinking/chapter11/holding/StackCollision.java
@@ -0,0 +1,27 @@
+package com.learn.thinking.chapter11.holding;
+
+import com.learn.thinking.chapter17.net.mindview.util.Stack;
+
+public class StackCollision {
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();
+        for (String s : "My dog has fleas".split(" ")) {
+            stack.push(s);
+        }
+        while (!stack.empty()) {
+            System.out.print(stack.pop() + " ");
+        }
+        System.out.println();
+        java.util.Stack<String> stack2 = new java.util.Stack<>();
+        for (String s : "My dog has fleas".split(" ")) {
+            stack2.push(s);
+        }
+        while (!stack2.empty()) {
+            System.out.print(stack2.pop() + " ");
+        }
+    }
+}
+/*Output:
+fleas has dog My
+fleas has dog My
+ */
---
+++ learn-thinkingjava/src/main/java/com/learn/thinking/chapter11/holding/StackTest.java
@@ -0,0 +1,19 @@
+package com.learn.thinking.chapter11.holding;
+
+import com.learn.thinking.chapter17.net.mindview.util.Stack;
+
+public class StackTest {
+    public static void main(String[] args) {
+        Stack<String> stack = new Stack<>();
+        for (String s : "My dog has fleas".split(" ")) {
+            stack.push(s);
+        }
+        while (!stack.empty()) {
+            System.out.print(stack.pop() + " ");
+        }
+    }
+}
+/*
+Output:
+fleas has dog My
+ */
