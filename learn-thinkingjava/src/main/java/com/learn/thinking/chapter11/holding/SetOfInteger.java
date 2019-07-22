38990cf4abf0aeab8c143ffb92c40e82405366cf
honglujun <909408954@qq.com>
2019-07-22 18:17:22 +0800

    11.9Set

--- learn-thinkingjava/src/main/java/com/learn/thinking/chapter11/holding/SetOfInteger.java
+++ learn-thinkingjava/src/main/java/com/learn/thinking/chapter11/holding/SetOfInteger.java
@@ -1,20 +1,26 @@
 package com.learn.thinking.chapter11.holding;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Random;
 import java.util.Set;
 
 public class SetOfInteger {
     public static void main(String[] args) {
         Random rand = new Random(47);
-        Set<Integer> intset = new HashSet<>();
-        for (int i = 0; i < 1000; i++) {
-            intset.add(rand.nextInt(30));
+        Set<Integer> intSet = new HashSet<>();
+        List<Integer> intList = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            intSet.add(rand.nextInt(10));
+            intList.add(rand.nextInt(10));
         }
-        System.out.println(intset);
+        System.out.println("Set:" + intSet);
+        System.out.println("List:" + intList);
     }
 }
 /*
 Output:
-[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]
+Set:[0, 1, 2, 3, 4, 6, 8, 9]
+List:[5, 1, 9, 0, 7, 8, 9, 8, 1, 8, 0, 2, 3, 5, 2, 4, 6, 6, 4, 0]
  */
