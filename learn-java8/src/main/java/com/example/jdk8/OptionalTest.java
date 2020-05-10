15f8c5e2df6f3370b7b1c2f05f8e3781127e6ca3
honglujun <909408954@qq.com>
2020-05-11 00:32:43 +0800

    java8的代码的修改

--- learn-java8/src/main/java/com/example/jdk8/OptionalTest.java
+++ learn-java8/src/main/java/com/example/jdk8/OptionalTest.java
@@ -16,13 +16,15 @@ public class OptionalTest {
         // 不确定对象是否为空
         Optional<String> optional2 = Optional.ofNullable("hello2");
 
-        // optional2如果存在就打印，不存在就什么都不做，如optional0
+        // optional2如果存在就打印，不存在就什么都不做，如optional0。
         // ifPresent()方法传递的是Consumer接口，
         // 这个接口是函数式接口，这里用的Lambda表达式来传递op -> System.out.println(op)
         // 这个Lambda表达式传递到这个抽象方法void accept(T t)，一个入参op，没有返回值
         // 函数式编程传递的是行为，不是值
-        optional2.ifPresent( op -> System.out.println(op));
-        optional0.ifPresent( op -> System.out.println(op));
+        optional2.ifPresent(op -> System.out.println(op));
+        // 等价于上面的代码
+        optional2.ifPresent(System.out::println);
+        optional0.ifPresent(op -> System.out.println(op));
 
         // optional2如果不存在就打印world2，
         System.out.println(optional2.orElse("world2"));
--- learn-java8/src/main/java/com/example/jdk8/Test3.java
+++ learn-java8/src/main/java/com/example/jdk8/Test3.java
@@ -1,53 +1,39 @@
 package com.example.jdk8;
 
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.function.Function;
-
 public class Test3 {
-
     public static void main(String[] args) {
+        /**
+         * 在将函数作为一等公民的语言中，Lambda表达式的类型是函数.
+         * 但在java中，Lambda表达式是对象，
+         * 他们必须依附于一类特别的的对象类型——函数式接口（functional interface）
+         */
+        MyInterface1 m1 = () -> {
+        };
+        System.out.println(m1.getClass().getInterfaces()[0]);
+        MyInterface2 m2 = () -> {
+        };
+        System.out.println(m2.getClass().getInterfaces()[0]);
 
-        TheInterface i1 = () -> {};
-        System.out.println(i1.getClass().getInterfaces()[0]);
-
-        TheInterface2 i2 = () -> {};
-        System.out.println(i2.getClass().getInterfaces()[0]);
-
-        new Thread(() -> System.out.println("hello world")).start();
-
-        List<String> list = Arrays.asList("hello", "world", "hello world");
-
-        list.forEach(item -> System.out.println(item.toUpperCase()));
-
-        List<String> list2 = new ArrayList<>();
-
-        list.forEach(item -> list2.add(item.toUpperCase()));
-        list2.forEach(item -> System.out.println(item));
-
-        //流式编程，map指的是映射，类似于hadoop的mapper.
-        list.stream().map(item -> item.toUpperCase()).forEach(item -> System.out.println(item));
-        list.stream().map(String::toUpperCase).forEach(item -> System.out.println(item));
-
-        Function<String, String> function = String::toUpperCase;
-        System.out.println(function.getClass().getInterfaces()[0]);
-
+        // Runnable接口是函数式接口，创建线程
+        new Thread(() -> {
+            System.out.println("hello world!");
+        }).start();
 
     }
 }
 
-
 @FunctionalInterface
-interface TheInterface {
-
-    void myMethod();
+interface MyInterface1 {
+    void method1();
 }
 
 @FunctionalInterface
-interface TheInterface2 {
-
-    void myMethod2();
+interface MyInterface2 {
+    void method2();
 }
 
+/*
+interface com.example.jdk8.MyInterface1
+interface com.example.jdk8.MyInterface2
+ */
